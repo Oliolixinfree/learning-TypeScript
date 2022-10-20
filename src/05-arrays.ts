aa8ec642615f5c326f44ca9444fb10903090095d
Ivan <offlane@tut.by>
2022-10-20 23:33:24 +0300

    Типизация массивов

---
+++ src/05-arrays.ts
@@ -0,0 +1,20 @@
+const numbers: number[] = [1, 2, 3, 4];
+const strs: string[] = [];
+const strs2: Array<string> = [];
+
+interface PC {
+  brand: string;
+  isNew: boolean;
+}
+
+const cars: PC[] = [];
+cars.push({ brand: 'Lenovo', isNew: false });
+
+const arrOfArr: string[][] = [];
+arrOfArr.push(['', '']);
+
+function printArr(arr: unknown[]): void {
+  arr.forEach((el, index) => {
+    console.log(el, index);
+  });
+}
