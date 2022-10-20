88da3e33fc8c5044911fc48cbeb8d80de9010d64
Ivan <vanish8ii@gmail.com>
2022-10-20 23:33:24 +0300

    Типизация массивов

---
+++ dist/05-arrays.js
@@ -0,0 +1,13 @@
+"use strict";
+const numbers = [1, 2, 3, 4];
+const strs = [];
+const strs2 = [];
+const cars = [];
+cars.push({ brand: 'Lenovo', isNew: false });
+const arrOfArr = [];
+arrOfArr.push(['', '']);
+function printArr(arr) {
+    arr.forEach((el, index) => {
+        console.log(el, index);
+    });
+}
