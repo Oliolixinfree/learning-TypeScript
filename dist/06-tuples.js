75361dca0186603acfd30c0e934cd2e7895cebe2
Ivan <offlane@tut.by>
2022-10-21 00:07:09 +0300

    Кортежи

---
+++ dist/06-tuples.js
@@ -0,0 +1,10 @@
+"use strict";
+// Кортежи
+const pairs = [
+    ['key', 'value'],
+    ['key2', 'value2'],
+];
+const data = ['str', true, 1];
+// csv
+const doc = [];
+doc.push(['Vasya', 'Petrov', 25, new Date(2020, 10, 10)]);
