efab019715f15205fb604e471ebf37097bdf82a7
Ivan <vanish8ii@gmail.com>
2022-10-21 00:07:09 +0300

    Кортежи

---
+++ src/06-tuples.ts
@@ -0,0 +1,11 @@
+// Кортежи
+const pairs: [string, string][] = [
+  ['key', 'value'],
+  ['key2', 'value2'],
+];
+
+const data: [string, boolean, number] = ['str', true, 1];
+
+// csv
+const doc: [string, string, number, Date][] = [];
+doc.push(['Vasya', 'Petrov', 25, new Date(2020, 10, 10)]);
