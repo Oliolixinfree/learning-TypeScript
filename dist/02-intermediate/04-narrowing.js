e64f0c2a137ffed0e54bad0a7da57884f06fe5fc
Ivan <offlane@tut.by>
2022-10-23 18:19:00 +0300

    Сужение типов

---
+++ dist/02-intermediate/04-narrowing.js
@@ -0,0 +1,40 @@
+"use strict";
+// Сужение типов
+function example1(x) {
+    if (typeof x === 'string') {
+        x.toUpperCase();
+    }
+    else if (typeof x === 'number') {
+        x.toFixed();
+    }
+    else if (x === undefined) {
+        console.log('no value');
+    }
+    else {
+        x;
+    }
+}
+function example2(strs) {
+    // if (typeof strs === 'object') {}
+    // Проверка на null
+    if (strs && typeof strs === 'object') {
+        strs.concat([]);
+    }
+    else if (typeof strs === 'string') {
+        strs.toLowerCase();
+    }
+}
+function example3(x) {
+    if (x instanceof Date) {
+        x.getMonth();
+    }
+    else {
+        x.concat([]);
+    }
+}
+function move(animal) {
+    if ('swim' in animal) {
+        return animal.swim();
+    }
+    return animal.fly();
+}
