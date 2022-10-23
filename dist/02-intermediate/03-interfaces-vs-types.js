e511f3bcea018195c59f8f3a65d5fc57ad8c34a3
Ivan <offlane@tut.by>
2022-10-23 16:41:31 +0300

    iterfaces vs types

---
+++ dist/02-intermediate/03-interfaces-vs-types.js
@@ -0,0 +1,7 @@
+"use strict";
+// Types
+// 1. primitives, union, intersections
+// type Window - can't be created
+// Interfaces
+// 1. When we want expand interface in future
+// 2. Class with implemenst
