690b95856aa59ae0296abd1a897035e202c173d8
Ivan <vanish8ii@gmail.com>
2022-10-27 00:41:09 +0300

    Шаблонные литералы(задача)

---
+++ dist/04-mixtypes/11-template-literals-task.js
@@ -0,0 +1,4 @@
+"use strict";
+// Создайте шаблонный литерал, соответствующий типам доступных значений для css-свойства border-style.
+let borderStyle = 'border: dashed black';
+let borderStyle2 = 'border: solid green';
